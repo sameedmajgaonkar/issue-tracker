dfff749c9afe5381458917e32f302261d155113a
Sameed Majgaonkar <sameed1718@gmail.com>
2025-03-21 00:26:45 +0530

    Style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import {
   NavigationMenu,
@@ -6,24 +8,38 @@ import {
   NavigationMenuLink,
 } from "@/components/ui/navigation-menu";
 import { AiFillBug } from "react-icons/ai";
-
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
 const NavBar = () => {
+  const pathName = usePathname();
+
+  const links = [
+    { href: "/", label: "Dashboard" },
+    { href: "/issues", label: "Issues" },
+  ];
   return (
     <NavigationMenu className="px-2 justify-start border-b min-w-full bg-accent text-3xl">
       <NavigationMenuList>
         <NavigationMenuItem>
           <AiFillBug />
         </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink asChild>
-            <Link href="/">Dashboard</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink asChild>
-            <Link href="/">Issues</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
+        {links.map(({ href, label }) => (
+          <NavigationMenuItem key={href}>
+            <NavigationMenuLink asChild>
+              <Link
+                href={href}
+                className={classNames(
+                  "text-neutral-500 hover:text-neutral-800 transition-colors font-semibold antialiased",
+                  {
+                    "text-neutral-950": href === pathName,
+                  }
+                )}
+              >
+                {label}
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        ))}
       </NavigationMenuList>
     </NavigationMenu>
   );
