ccbb214c1670625c95da6a1cb0cce676f02d3d6a
Sameed Majgaonkar <sameed1718@gmail.com>
2025-03-22 12:28:34 +0530

    Handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,48 @@
 "use client";
+import { useForm, Controller } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import dynamic from "next/dynamic";
+import axios from "axios";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { FaGithubAlt } from "react-icons/fa6";
-import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
 const NewIssuePage = () => {
+  const router = useRouter();
+
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
+  const doSubmit = async (issue: IssueForm) => {
+    await axios.post("/api/issues", issue);
+    router.push("/issues");
+  };
+
   return (
-    <div className="space-y-3 min-w-full md:w-1/2 overflow-hidden">
-      <Input type="text" placeholder="Title" />
-      <SimpleMDE />
+    <form
+      className="space-y-3 md:px-32 lg:px-48"
+      onSubmit={handleSubmit(doSubmit)}
+    >
+      <Input type="text" placeholder="Title" {...register("title")} />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => <SimpleMDE {...field} />}
+      />
       <Button className="rounded-sm">
         <FaGithubAlt />
         Submit New Issue
       </Button>
-    </div>
+    </form>
   );
 };
 
