c349dec837dd5992b2f5e7ac12c467d1cab2df4f
Sameed Majgaonkar <sameed1718@gmail.com>
2025-03-22 16:26:44 +0530

    Display all the issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,51 @@
 import React from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-const IssuesPage = () => {
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { prisma } from "@/prisma/client";
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
+
   return (
-    <div>
-      <Button asChild variant="outline">
-        <Link href="/issues/new">New Issue</Link>
-      </Button>
-    </div>
+    <>
+      <div className="mb-10">
+        <Button asChild variant="outline">
+          <Link href="/issues/new">New Issue</Link>
+        </Button>
+      </div>
+      <div className="md:px-56">
+        <Table className="text-center">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="text-center">Issue</TableHead>
+              <TableHead className="text-center">Status</TableHead>
+              <TableHead className="text-center hidden md:table-cell">
+                Created
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {issues.map((issue) => (
+              <TableRow key={issue.id}>
+                <TableCell>{issue.title}</TableCell>
+                <TableCell>{issue.status}</TableCell>
+                <TableCell className="hidden md:table-cell">
+                  {issue.createdAt.toDateString()}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </>
   );
 };
 
