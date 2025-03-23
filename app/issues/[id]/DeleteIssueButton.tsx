e1296c6295fce770d989de21450ffbea870d759a
Sameed Majgaonkar <sameed1718@gmail.com>
2025-03-24 00:28:28 +0530

    Show a confirmation dialog box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,5 +1,16 @@
 import { Button } from "@/components/ui/button";
 import { MdDelete } from "react-icons/md";
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 
 interface Props {
   issueId: number;
@@ -7,9 +18,29 @@ interface Props {
 
 const DeleteIssueButton = ({ issueId }: Props) => {
   return (
-    <Button className="bg-destructive">
-      <MdDelete /> Delete Issue
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button className="bg-destructive">
+          <MdDelete /> Delete Issue
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            <MdDelete />
+            Delete Issue
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            Once deleted, it cannot be recoverd. Are you sure you want to
+            permanently delete this issue?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogAction>Proceed</AlertDialogAction>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 
